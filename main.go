94330f1b4f61c46063ca9255f1e8b6689160bb02
Spitfire1970 <ngoyal7019@gmail.com>
2025-11-30 18:09:58 +0000

    init

---
+++ main.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+)
+
+const (
+	host = "0.0.0.0"
+	port = "22"
+)
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			activeterm.Middleware(), // Bubble Tea apps usually require a PTY.
+			logging.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+}
+
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	// This should never fail, as we are using the activeterm middleware.
+	pty, _, _ := s.Pty()
+
+	renderer := bubbletea.MakeRenderer(s)
+	mainStyle := renderer.NewStyle().MarginLeft(2)
+	checkboxStyle := renderer.NewStyle().Bold(false).Foreground(lipgloss.Color("213"))
+	aboutStyle := renderer.NewStyle().Bold(true).Foreground(lipgloss.Color("246"))
+	aboutNameStyle := renderer.NewStyle().Bold(true).Foreground(lipgloss.Color("15"))
+	subtleStyle := renderer.NewStyle().Foreground(lipgloss.Color("241"))
+	dotStyle := renderer.NewStyle().Foreground(lipgloss.Color("236")).Render(dotChar)
+
+	m := model{
+		Width:          pty.Window.Width,
+		Height:         pty.Window.Height,
+		Choice:         0,
+		Chosen:         false,
+		mainStyle:      mainStyle,
+		aboutStyle:     aboutStyle,
+		aboutNameStyle: aboutNameStyle,
+		checkboxStyle:  checkboxStyle,
+		subtleStyle:    subtleStyle,
+		dotStyle:       dotStyle,
+		sess:           s,
+		runtime:        "",
+	}
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+const (
+	dotChar = " • "
+
+	RESUME_URL   = "https://nakul.one/resume" 
+ 	GITHUB_URL   = "https://github.com/Spitfire1970"
+	LINKEDIN_URL = "https://www.linkedin.com/in/nakul404/"
+	LEETCODE_URL  = "https://leetcode.com/u/Spitfire1970"
+)
+
+// Just a generic tea.Model to demo terminal information of ssh.
+type model struct {
+	Width          int
+	Height         int
+	Choice         int
+	Chosen         bool
+	mainStyle      lipgloss.Style
+	aboutStyle     lipgloss.Style
+	aboutNameStyle lipgloss.Style
+	checkboxStyle  lipgloss.Style
+	subtleStyle    lipgloss.Style
+	dotStyle       string
+	sess           ssh.Session
+	runtime        string
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+type openNextRuntime struct{}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.Width = msg.Width
+		m.Height = msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "j", "down":
+			m.Choice++
+			if m.Choice > 3 {
+				m.Choice = 3
+			}
+		case "k", "up":
+			m.Choice--
+			if m.Choice < 0 {
+				m.Choice = 0
+			}
+		case "enter":
+			m.runtime = "linux"
+			return openByChoice(m)
+		}
+	case openNextRuntime:
+		switch m.runtime {
+		case "linux":
+			m.runtime = "darwin"
+			return openByChoice(m)
+		case "darwin":
+			m.runtime = "windows"
+			return openByChoice(m)
+		default:
+			m.runtime = ""
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+
+	about := m.aboutStyle.Render(fmt.Sprintf(strings.TrimSpace(`
+Hi I'm %s,
+
+A software engineer specialized in many software domains
+including Web, Backend, ML (not just LLMs), and, DevOps.
+
+On weekends, I am... a recreational programmer.
+
+I'm fluent in Scala, Python, Go, and, Typescript. Can work with C.
+`), m.aboutNameStyle.Render("Nakul Goyal")))
+
+	tpl := m.subtleStyle.Render("Hint: q, ctrl+c: quit")
+
+	choices := fmt.Sprintf(
+		"%s\n%s\n%s\n%s",
+		m.subtleStyle.Copy().Foreground(lipgloss.Color("222")).Render("Resume / CV    https://nakul.one/resume"),
+		m.subtleStyle.Copy().Foreground(lipgloss.Color("13")).Render("GitHub         https://github.com/Spitfire1970"),
+		m.subtleStyle.Copy().Foreground(lipgloss.Color("33")).Render("Linkedin       https://linkedin.com/in/nakul404"),
+		m.subtleStyle.Copy().Foreground(lipgloss.Color("39")).Render("CP        https://leetcode.com/u/Spitfire1970/"),
+	)
+
+	s := fmt.Sprintf("%s\n\n%s\n\n%s", about, choices, tpl)
+	return m.mainStyle.Render("\n" + s + "\n\n")
+}
+
+func checkbox(checkboxStyle lipgloss.Style, label string, checked bool) string {
+	if checked {
+		return checkboxStyle.Render("[x] " + label)
+	}
+	return fmt.Sprintf("[ ] %s", label)
+}
+
+func openByChoice(m model) (tea.Model, tea.Cmd) {
+	switch m.Choice {
+	case 0:
+		return m, openURL(m, RESUME_URL)
+	case 1:
+		return m, openURL(m, GITHUB_URL)
+	case 2:
+		return m, openURL(m, LINKEDIN_URL)
+	case 3:
+		return m, openURL(m, LEETCODE_URL)
+	}
+	return m, nil
+}
+
+func openURL(m model, url string) tea.Cmd {
+	var cmd string
+	var args []string
+
+	runtime := m.runtime
+
+	switch runtime {
+	case "linux":
+		cmd = "xdg-open"
+	case "darwin":
+		cmd = "open"
+	default:
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	}
+	args = append(args, url)
+	c := wish.Command(m.sess, cmd, args...)
+
+	cmdExec := tea.Exec(c, func(_ error) tea.Msg {
+
+		return openNextRuntime{}
+	})
+	return cmdExec
+}
